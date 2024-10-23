1103b5b4f6423be1c14baacff49c54ad30304a31
hari7699 <hariharavarmaaketi@gmail.com>
2024-10-22 22:26:59 -0500

    Added Coin Model

---
+++ src/main/java/com/crypto/model/Coin.java
@@ -0,0 +1,90 @@
+package com.crypto.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Coin {
+
+    @Id
+    @JsonProperty("id")
+    public String id;
+
+    @JsonProperty("symbol")
+    public String symbol;
+
+    @JsonProperty("name")
+    public String name;
+
+    @JsonProperty("image")
+    public String image;
+
+    @JsonProperty("current_price")
+    public double currentPrice;
+
+    @JsonProperty("market_cap")
+    public long marketCap;
+
+    @JsonProperty("market_cap_rank")
+    public int marketCapRank;
+
+    @JsonProperty("fully_diluted_valuation")
+    public long fullyDilutedValuation;
+
+    @JsonProperty("total_volume")
+    public long totalVolume;
+
+    @JsonProperty("high_24h")
+    public double high24h;
+
+    @JsonProperty("low_24h")
+    public double low24h;
+
+    @JsonProperty("price_change_24h")
+    public double priceChange24h;
+
+    @JsonProperty("price_change_percentage_24h")
+    public double priceChangePercentage24h;
+
+    @JsonProperty("market_cap_change_24h")
+    public long marketCapChange24h;
+
+    @JsonProperty("market_cap_change_percentage_24h")
+    public double marketCapChangePercentage24h;
+
+    @JsonProperty("circulating_supply")
+    public long circulatingSupply;
+
+    @JsonProperty("total_supply")
+    public long totalSupply;
+
+    @JsonProperty("max_supply")
+    public long maxSupply;
+
+    @JsonProperty("ath")
+    public double ath;
+
+    @JsonProperty("ath_change_percentage")
+    public double athChangePercentage;
+
+    @JsonProperty("ath_date")
+    public String athDate;
+
+    @JsonProperty("atl")
+    public double atl;
+
+    @JsonProperty("atl_change_percentage")
+    public double atlChangePercentage;
+
+    @JsonProperty("atl_date")
+    public String atlDate;
+
+    @JsonProperty("roi")
+    public Object roi;  // You can replace Object with a more specific type if needed
+
+    @JsonProperty("last_updated")
+    public String lastUpdated;
+}
